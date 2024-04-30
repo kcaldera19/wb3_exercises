4b22e74b4625c56c6cf458fe1d456405703e8f14
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-30 15:07:19 -0500

    more function exercises

--- functions/more_functions.js
+++ functions/more_functions.js
@@ -21,25 +21,25 @@ function addNumbers(num1,num2){
 function displayReceipt(totalDue,amountPaid){
     const changeDue= amountPaid - totalDue ;
     
-    if (amountPaid< totalDue) {
+    if (amountPaid < totalDue) {
        console.log ("Amount paid is less than the total amount due");
-    }else if(amountPaid > totalDue){
+    }
+    else if(amountPaid > totalDue){
         console.log("Amount paid is more than the total amount due");
-
-    }else{
+    }
+    else{
         console.log(`Total Due: $${totalDue.toFixed(2)} Amount Paid: $${amountPaid.toFixed(2)} Change Due:$${Math.abs(changeDue).toFixed(2)}`);
 
     }
 
-
-    
-
-
-
 }
+
 displayMailingLabel("kathy","10101 Abc Street","Lake State","TX", 23232);
 displayMailingLabel("wow","1400 RoadMap","WoW Lake","TX",12345 );
+
 addNumbers(5,10);
 addNumbers(20,5);
+
 displayReceipt(20,30);
 displayReceipt(50,30);
+displayReceipt(100,100);
