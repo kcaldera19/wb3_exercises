2efb6a8904aa42390518eeba48d3e6715438d8e9
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-30 11:54:41 -0500

    simple function

---
+++ functions/simple_function.js
@@ -0,0 +1,20 @@
+"use strict"
+
+function favoriteThing(){
+
+    console.log("Katalina")
+    console.log("LOTR")
+    console.log("Vampire Weekend")
+}
+favoriteThing();
+
+
+function whyImHere(){
+    console.log("To try something new")
+}
+whyImHere();
+
+function favoritePlace(){
+    console.log("I like going to the museum and festivals because they are cool.")
+}
+favoritePlace();
