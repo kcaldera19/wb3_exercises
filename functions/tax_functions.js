3194196a0ef176043004dd0c3386214cb9db005e
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-01 11:23:47 -0500

    tax function

--- functions/tax_functions.js
+++ functions/tax_functions.js
@@ -5,7 +5,7 @@ function getSocSecTax(grosspay){
    const socialTax = grosspay * taxeRate;
    return socialTax;
 }
-console.log(getSocSecTax(1000));
+console.log("Social Security Tax is " + getSocSecTax(1000));
 
 function getMedicareTax(grosspay){
     const taxeRate = (1.45/100);
@@ -13,19 +13,38 @@ function getMedicareTax(grosspay){
 
     return medicareTax;
 }
-console.log(getMedicareTax(1000));
+console.log("Medical Care Tax is " + getMedicareTax(1000));
 
 function getFederalTax(grosspay,withholdingCode){
-    const federalTax = grosspay * withholdingCode;
+    
+    let taxRate = 0;
 
-
-    if (withholdingCode ===  23){
-        console.log(`person1: gross pay$${grosspay} withholding code${0}`);
-    }else if(withholdingCode === 21){
-        console.log(`person2:  `)
+    
+    if (withholdingCode ===  0){
+        taxRate =23/100;
+        
     }
+     else if(withholdingCode === 1){
+        taxRate = 21/100;
+         
+    }
+    else if(withholdingCode === 2){
+        taxRate = 19.5/100;
     
+    } else if(withholdingCode === 3){
+        taxRate = 18.5/100;
+       
+    }
+    else if (withholdingCode >= 4){
+        taxRate = .18-((withholdingCode-4)*.005);
+       
+    }
+
+    const federalTax = (grosspay * taxRate)/100;
     
+    console.log(`gross pay$${grosspay} withholding code${withholdingCode} the ${taxRate} tax return`);
     return federalTax;
+    
+    
 }
-getFederalTax(750,23);
+getFederalTax();
