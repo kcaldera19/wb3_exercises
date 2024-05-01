d993caee202d31322233dfa1f394524b663b19fc
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-30 22:20:56 -0500

    tax function for getmedical tax

--- functions/tax_functions.js
+++ functions/tax_functions.js
@@ -1,21 +1,31 @@
 "use strict"
 
 function getSocSecTax(grosspay){
-    const taxeRate = 6.2;
-    const socialTax = grosspay*(6.2/100);
-    
-    return socialTax;
+   const taxeRate = (6.2/100);
+   const socialTax = grosspay * taxeRate;
+   return socialTax;
 }
-console.log(getSocSecTax);
+console.log(getSocSecTax(1000));
 
 function getMedicareTax(grosspay){
-    const taxeRate =  
+    const taxeRate = (1.45/100);
+    const medicareTax = grosspay * taxeRate;  
 
-    return MedicareTax
+    return medicareTax;
 }
+console.log(getMedicareTax(1000));
 
-function getFederalTax(grosspay){
+function getFederalTax(grosspay,withholdingCode){
+    const federalTax = grosspay * withholdingCode;
+
+
+    if (withholdingCode ===  23){
+        console.log(`person1: gross pay$${grosspay} withholding code${0}`);
+    }else if(withholdingCode === 21){
+        console.log(`person2:  `)
+    }
     
     
-    return federalTax
+    return federalTax;
 }
+getFederalTax(750,23);
