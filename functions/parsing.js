a7e14986ec000404e99f6ca95c9d83899060a4d3
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-01 21:07:46 -0500

    added parsing.js

---
+++ functions/parsing.js
@@ -0,0 +1,13 @@
+"use strict"
+
+
+
+
+let name ="Brenda Kaye";
+// let first = "firstName".indexOf();
+// let last = "lastName".indexOf();
+// let result = "name".indexOf("");
+// console.log(result);
+
+
+
