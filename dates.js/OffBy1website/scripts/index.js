12f210dda4dde040370dce4192a0652b23012cc0
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-03 11:33:02 -0500

    added some comments

--- dates.js/OffBy1website/scripts/index.js
+++ dates.js/OffBy1website/scripts/index.js
@@ -27,3 +27,17 @@ function displayTime(){
 
     theParagraph.innerHTML = generatedDate.toLocaleString();
 }
+
+
+// let d = new Date("09/09/3003");
+// console.lod(d.toString());
+
+
+// miliseconds
+// let g = Date.parse("08-30-2005");
+// console.log(d);
+
+
+// let milliSec = Date.parse("May 1, 2022");
+// let d = new Date(milliSec);
+
